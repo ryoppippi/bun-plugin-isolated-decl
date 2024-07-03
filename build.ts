86449c592c49ef165e4c9811f04046f08643cb51
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-03 14:47:32 +0100

    feat(build.ts): simplify external dependencies extraction
    This commit simplifies the way we extract external dependencies from
    the package.json file. Instead of creating separate arrays for each
    type of dependency and then spreading them into a new array, we now
    spread the dependencies directly into a new object and then get the keys.
    This results in cleaner and more efficient code.

--- build.ts
+++ build.ts
@@ -2,11 +2,11 @@ import dts from './index.ts';
 
 /* eslint-disable ts/no-unsafe-assignment, ts/no-unsafe-member-access, ts/no-unsafe-argument */
 const pkgJson = await Bun.file('./package.json').json();
-const external = [
-	...Object.keys(pkgJson?.dependencies ?? {}),
-	...Object.keys(pkgJson?.devDependencies ?? {}),
-	...Object.keys(pkgJson?.peerDependencies ?? {}),
-];
+const external = Object.keys({
+	...pkgJson?.dependencies,
+	...pkgJson?.devDependencies,
+	...pkgJson?.peerDependencies,
+});
 /* eslint-enable ts/no-unsafe-assignment, ts/no-unsafe-member-access, ts/no-unsafe-argument */
 
 await Bun.build({
